8f9c6e8b5127fed2e392bd06ce288911d748c5c0
SHAILESH GOWTHAM V <shaileshgowtham7@gmail.com>
2026-07-09 21:37:51 +0530

    Delete src directory

--- src/verifiers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Makes verifiers a package
-from .math import verify_math_answer
-from .code import verify_code_answer
-from .ner import verify_ner_answer
-from .format import verify_summary
-
-__all__ = ["verify_math_answer", "verify_code_answer", "verify_ner_answer", "verify_summary"]
